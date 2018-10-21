6fae4c514f81c2a539f5e75988c795c41ada981a
raf willems <raf.willems@vrt.be>
2018-10-21 15:36:14 +0200

    Initial commit

---
+++ app/src/main/java/be/rafwillems/beerpoc/MainActivity.kt
@@ -0,0 +1,33 @@
+package be.rafwillems.beerpoc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.bluelinelabs.conductor.Router
+import com.bluelinelabs.conductor.RouterTransaction
+import com.bluelinelabs.conductor.Conductor
+import android.view.ViewGroup
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var router: Router
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val container = findViewById(R.id.controller_container) as ViewGroup
+
+        router = Conductor.attachRouter(this, container, savedInstanceState)
+        if (!router.hasRootController()) {
+            router.setRoot(RouterTransaction.with(HomeController()))
+        }
+    }
+
+    override fun onBackPressed() {
+        if (!router.handleBack()) {
+            super.onBackPressed()
+        }
+    }
+
+}
